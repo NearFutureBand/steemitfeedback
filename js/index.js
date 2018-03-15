f424bd483d85400c8666f4118d40b4ae8abe7494
SharkyInstigator <emberprime43@gmail.com>
2018-03-15 23:54:54 +0300

    13
    Done with nav, except "Get my files"

--- js/index.js
+++ js/index.js
@@ -5,6 +5,43 @@ buttonAddNote.onclick = function(){
     form.style.display = "block";
     buttonAddNote.style.display = "none";
 }
-document.getElementById("butFormDone").onclick = function(){
-    //form.style.display = "none";
-}
+
+async function getUrls() {
+        if (wif == '') {
+            await auth();
+        } else {
+            golos.api.getContent(username, constPermlik, function(err, result) {
+                result.id == 0 ? swal({
+                    html: document.getElementById('no-records-IPFS').innerHTML
+                }) : getPostJson(username, constPermlik, result);
+                if (err) swal(err);
+            });
+        }
+    }
+document.getElementById('golos-urls').onclick = getUrls;
+
+document.getElementById('aboutGolosImagesCallBtn').addEventListener('click', () => {
+        swal({
+            title: document.getElementById('about-html-title').innerHTML,
+            html: document.getElementById('about-html').innerHTML,
+            type: 'info',
+            buttonsStyling: false,
+            confirmButtonClass: 'btn btn-success btn-lg',
+            confirmButtonText: document.getElementById('button-cool').innerHTML,
+            position: 'top',
+            showCloseButton: true
+        });
+    }, false);
+
+document.getElementById('integration').addEventListener('click', function(e) {
+        swal({
+            title: 'About integration!',
+            html: document.getElementById('integration-html').innerHTML,
+            type: 'info',
+            buttonsStyling: false,
+            confirmButtonClass: 'btn btn-success btn-lg',
+            confirmButtonText: '<span class="icon-checkmark"></span> Cool!',
+            position: 'top',
+            showCloseButton: true
+        })
+    })
