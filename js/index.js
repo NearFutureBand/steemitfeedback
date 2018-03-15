923ec2cc49d29a8051f9ff69b0fbe576e5b630a3
SharkyInstigator <emberprime43@gmail.com>
2018-03-15 20:00:01 +0300

    15
    Adding header&footer, file for scripts

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+var form = document.getElementById("formAddNote");
+document.getElementById("butAddNote").onclick = function(){
+    form.style.display = "block";
+}
+document.getElementById("butFormDone").onclick = function(){
+    form.style.display = "none";
+}
