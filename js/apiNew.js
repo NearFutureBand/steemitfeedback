35e49afdbfb38237e157741531d012c9f71ea13e
Paul White <emberprime43@gmail.com>
2018-04-30 01:21:14 +0300

    pre final packing of the new api.js

--- js/apiNew.js
+++ js/apiNew.js
@@ -24,16 +24,16 @@ var initGolosFeedback = function(){
 document.addEventListener('DOMContentLoaded', initGolosFeedback);
 
 
-//START
-
+//START---------------------------------------------------------------------------------
 var initBootstrapStructure = function(){
     let wrapper = document.querySelector('.'+prefix+'wrapper');
     wrapper.classList.add('container');
 }
 
 
-//TABS
 
+
+//TABS----------------------------------------------------------------------------------
 var initTabs = function(){
     let navTabs = document.createElement('div');
     navTabs.className = 'row nav-tab-buttons';
@@ -67,32 +67,83 @@ var createFromAddFb = function(){
     addEventForFbDone();
     addEventForFbCancel();
 }
-function addEventForFbDone(){
+var addEventForFbDone = function(){
     document.querySelector('.'+prefix+'wrapper .frm-add-fb').getElementsByTagName('form')[0].addEventListener('submit', function(e){
         e.preventDefault();
-        if(wif){
+        /*if(wif){
             sendAddFbForm();
         }else{
             auth(sendAddFbForm);
         }
-        return false;
+        return false;*/
     });
 }
-function addEventForFbCancel(){
+var sendAddFbForm = function(){
+    //wif test3 testnet1 5Hvp79CaQrYUD9d33VvdtWY5BhyimS4t5vMDCBJE1WsTUUPuu1F";
+    let parentAuthor = '';
+    let parentPermlink = 'fb';
+    //let author = username;
+    let title = document.getElementById('formHeader').value;
+    let permlink = 'post-' + parentPermlink.split(' ')[0] + '-' + Date.now().toString();
+    let body = document.getElementById('formText').value;
+    let tagList = {
+        tags: [findCheckedRadio()]
+    };
+    let jsonMetadata = JSON.stringify(tagList);
+    
+    console.log('title: '+title+' body: '+body+' tags: '+parentPermlink+' '+tagList+' permlink: '+permlink);
+    //console.log(window.wif);
+    /*golos.broadcast.comment(wif, parentAuthor, parentPermlink, author, permlink, title, body, jsonMetadata, function(err, result) {
+        //console.log(err, result);
+        if ( ! err) {
+            console.log('comment', result);
+            golos.api.getContent(author, permlink, function(err, result) {
+                //console.log(err, result);
+                if (!err) {
+                    console.log(result);
+                    createNote(formData(result));
+                    toggleBtnCom(noteId);
+                }
+                else console.error(err);
+            });
+        }
+        else console.error(err);
+    });*/
+    
+    document.getElementById('formHeader').value = '';
+    document.getElementById('formText').value = '';
+    closeAddFbForm();
+    
+    //getContent and ONLY AFTER loadNotes();
+    
+    document.querySelector('.lding').style.display = 'none';
+    //SHOW MESSAGE ABOUT SUCCESSFUL SENDING
+}
+var addEventForFbCancel = function(){
     document.querySelector('.'+prefix+'wrapper .frm-add-fb .btn-add-fb-cancel').addEventListener('click',function(){
         closeAddFbForm();
     });
 }
-function openAddFbForm(){
+var openAddFbForm = function(){
     createFromAddFb();
     document.querySelector('.'+prefix+'btn-add-fb').style.display = 'none';
     removeFbs();
 }
-function closeAddFbForm(){
+var closeAddFbForm = function(){
     document.querySelector('.'+prefix+'wrapper .frm-add-fb').remove();
     document.querySelector('.'+prefix+'btn-add-fb').style.display = 'block';
     loadFbs();
 }
+var findCheckedRadio = function(){
+    let res = '';
+    Array.from(getBlockAddNote().getElementsByClassName('form-check-input')).forEach(function(item){
+        if(item.checked==true){
+            res = item.getAttribute('id').split('-')[1].toString();
+        }
+    });
+    return res;
+}
+
 
 
 
@@ -165,87 +216,35 @@ var createFb = function(data){
     checkVoteColor(data[0],'');
     
     addEventsForCommentButtons(data[0]);
-    //addEventsForFbLikes(data[0]);
-    //addEventForFbHeader(data[0]);
+    addEventsForFbLikes(data[0]);
+    addEventForFbHeader(data[0]);
     
     console.log('feedback has been created: id = '+data[0]);
 }
-
 var addEventsForCommentButtons = function (fbId){
     getBtnShowComment(fbId).addEventListener('click',function(){
-        if(document.getElementById(fbId).getAttribute('data-opened') == '0'){
-            reloadNote(fbId,true,true,true);            
-        }else{
-            removeFbs();
-            loadFbs();
-        }
+        toggleFb(fbId);
     });
 }
-var reloadNote = function(fbId,expanded,loading,removeAll){
-    
-    // on/off loading animation
-    /*if(loading==true){
-        document.querySelector('.lding').style.display = 'block';
-    }*/
-    
-    let permlink = getPermlink(fbId,'');
-    let author = getAuthor(fbId,'');
-    
-    //remove all - for the expanding mode, remove one - for the reloading mode
-    if(removeAll==true){
-        removeFbs();
-    }else{
-        removeFb(fbId);
-    }
-    
-    golos.api.getContent(author, permlink, function(err, result) {
+var expandFb = function(fbId){
+    golos.api.getContent(getAuthor(fbId,''), getPermlink(fbId,''), function(err, result) {
         //console.log(err, result);
         if (!err) {
+            removeFbs();
             console.log(result);
             createFb(formData(result));
             toggleBtnCom(fbId);
-            
-            //if the note was expanded
-            if(expanded==true){
-                document.getElementById(fbId).setAttribute('data-opened',1);
-                loadComments(fbId);
-                createCommentForm(fbId);
-            }
+            document.getElementById(fbId).setAttribute('data-opened',1);
+            loadComments(fbId);
+            createCommentForm(fbId);
         }
         else console.error(err);
     });
-    
-    /*if(loading==true){
-        document.querySelector('.lding').style.display = 'none';
-    }*/
 }
-
-function removeFbs(){
+var removeFbs = function(){
     document.querySelector('.'+prefix+'wrapper .fb').remove();
 }
-function getVoteState(fbId,comId){
-    let state;
-    if(comId){
-        state = Number(getComment(fbId,comId).getAttribute('data-like'));
-    }else{
-        state = Number(document.getElementById(fbId).getAttribute('data-like'));
-    }
-    return state;
-}
-function getVoteStateOnload(object){
-    let result=0;
-    /*if(wif){
-        object.active_votes.forEach(function(item){
-            if(item.voter==username){
-                result = item.percent;
-            }
-        });
-    }else{
-        result = 0;
-    }*/
-    return result;
-}
-function checkVoteColor(fbId,comId){
+var checkVoteColor = function(fbId,comId){
     let state = getVoteState(fbId,comId);
     if(state == -1){
         delClassIfContains(getBtnVote(fbId,comId,1),'btn-success');
@@ -258,10 +257,7 @@ function checkVoteColor(fbId,comId){
         getBtnVote(fbId,comId,1).classList.add('btn-success');
     }
 }
-function getBtnShowComment(fbId){
-    return document.getElementById(fbId).getElementsByClassName('btn-show-comments')[0];
-}
-function toggleBtnCom(fbId){
+var toggleBtnCom = function(fbId){
     let thisBtn = getBtnShowComment(fbId);
     if(thisBtn.children[2].classList.contains('hidden')){
         thisBtn.children[0].classList.add('hidden');
@@ -275,13 +271,38 @@ function toggleBtnCom(fbId){
         thisBtn.children[1].classList.remove('hidden');
     }
 }
-function getFbControls(fbId){
+var addEventForFbHeader = function(fbId){
+    getFbHeader(fbId).addEventListener('click',function(){
+        toggleFb(fbId);
+    });
+}
+var toggleFb = function(fbId){
+    if(document.getElementById(fbId).getAttribute('data-opened') == '0'){
+        expandFb(fbId);
+    }else{
+        removeFbs();
+        loadFbs();
+    }
+}
+
+var getBtnShowComment = function(fbId){
+    return document.getElementById(fbId).getElementsByClassName('btn-show-comments')[0];
+}
+var getFbControls = function(fbId){
     return document.getElementById(fbId).getElementsByClassName('controls')[0];
 }
+var getFbHeader = function(fbId){
+    return document.getElementById(fbId).getElementsByClassName('text')[0].children[0];
+}
+//prototype getFbById(fbId) - take fb from existing here
+//prototype getFbByPermlink(permlink) - take fb from the blockchain
+
+
+
 
 
 //COMMENTS-------------------------------------------------------------------------------
-function loadComments(fbId){
+var loadComments = function(fbId){
     golos.api.getContentReplies(getAuthor(fbId,''), getPermlink(fbId,''), function(err, result) {
         //console.log(err, result);
         if (!err) {
@@ -320,12 +341,12 @@ var createComment = function(data){
     comment.setAttribute('data-like',data[8]);
     comment.innerHTML = "<div class='col-lg-10 offset-lg-1 col-md-10 offset-md-1 tile body-comment'><div class='row'><div class='col-lg-9 col-md-9 text'><p>"+data[2]+"</p></div><div class='col-lg-3 col-md-3 controls'><div class='controls-wrapper'><div class='name'><h6>"+data[3]+"</h6></div><div class='date'><small>"+data[4]+"</small></div><div class='likes'><span>"+data[5]+"</span><button type='button' class='btn btn-secondary btn-com-vote' data-like='1'><span class='icon-thumbs-up'></span></button><button type='button' class='btn btn-secondary btn-com-vote' data-like='0'><span class='icon-thumbs-down'></span></button><span>"+data[6]+"</span></div></div></div></div></div>";
     getBlockComments(data[1]).appendChild(comment);
-    //checkVoteColor(data[1],data[0]);
-    //addEventsForComLikes(data[1],data[0]);
+    checkVoteColor(data[1],data[0]);
+    addEventsForComLikes(data[1],data[0]);
     console.log("comment has been created: "+data[1]+" "+data[0]);
 }
 
-function getComment(fbId, comId){
+var getComment = function(fbId, comId){
     let comment;
     Array.from(getBlockComments(fbId).children).forEach(function(item){
         if(item.getAttribute('id')==comId){
@@ -334,10 +355,12 @@ function getComment(fbId, comId){
     });
     return comment;
 }
-
-function getBlockComments(noteId){
-    return document.getElementById(noteId).getElementsByClassName('comments')[0];
+var getBlockComments = function(fbId){
+    return document.getElementById(fbId).getElementsByClassName('comments')[0];
 }
+
+
+
 
 
 //FORM FOR ADDING NEW COMMENT-------------------------------------------------------------
@@ -346,13 +369,93 @@ function createCommentForm(fbId){
     commentForm.className = 'container frm-add-com';
     commentForm.innerHTML = "<div class='row'><div class='col-lg-10 offset-lg-1 col-md-10 offset-md-1 tile'><form><div class='form-group'><textarea class='form-control txt-add-com' id='commentBody' rows='3' placeholder='Type your comment here' required></textarea></div><button type='click' class='btn btn-primary btn-add-com-done'><span class='icon-checkmark'></span> Submit</button></form></div></div>";
     document.getElementById(fbId).appendChild(commentForm);
-    //addEventsForComDone(noteId);
+    addEventsForComDone(noteId);
+}
+var addEventsForComDone = function(fbId){
+    getAddComForm(fbId).addEventListener('submit',function(e){
+        e.preventDefault();
+        /*if(wif){
+            sendAddComForm(fbId);
+        }else{
+            auth(sendAddComForm.bind(this, fbId));
+        }*/
+        return false;
+    });
+}
+var sendAddComForm = function(fbId){
+    let parentAuthor = getAuthor(fbId,'');
+    let parentPermlink = getPermlink(fbId,'');
+    //let author = username;
+    let permlink = 're-' + parentAuthor + '-' + parentPermlink + '-' + Date.now();
+    let title = '';
+    let body = getTxtareaCom(noteId).value;
+    let jsonMetadata = '{}';
+    console.log('comment to note '+noteId+'. Body: '+body);
+    /*golos.broadcast.comment(wif, parentAuthor, parentPermlink, author, permlink, title, body, jsonMetadata, function(err, result) {
+        //console.log(err, result);
+        if (!err) {
+            console.log('comment', result);
+        }
+        else console.error(err);
+    });*/
+    getTxtareaCom(noteId).value = '';
+}
+
+var getTxtareaCom = function(fbId){
+    return getBlockFormAddComment(fbId).getElementsByClassName('txt-add-com')[0];
+}
+var getBlockFormAddComment = function(fbId){
+    return document.getElementById(fbId).children[document.getElementById(fbId).childElementCount-1];
 }
 
 
 
 
-//VOTES
+//VOTING----------------------------------------------------------------------------------
+//эти 4 функции похожи
+var addEventsForFbLikes = function(fbId){
+    getBtnsVote(fbId,'').forEach(function(item){
+        item.addEventListener('click', function(){
+            let isLike = Number(item.getAttribute('data-like'));
+            /*if(wif){
+                voteForNote(fbId,isLike);
+            }else{
+                auth(voteForNote.bind(this, fbId,isLike));
+            }*/
+        });
+    });
+}
+var voteForFb = function(fbId, like){
+    let weight;
+    (like == 1)? weight = 10000 : weight = -10000;
+    weight = updateVoteState(fbId,'',weight/10000);
+    /*golos.broadcast.vote(wif, username, getNoteAuthor(fbId), getPermlink(fbId,''), weight, function(err, result) {
+        console.log(err, result);
+        if ( ! err) {
+            //callback here - вызов функции покраски
+        }
+    });*/
+}
+var addEventsForComLikes = function(fbId, comId){
+    getBtnsVote(noteId,comId).forEach(function(item){
+        item.addEventListener('click', function(){
+            let isLike = Number(item.getAttribute('data-like'));
+            /*if(wif){
+                voteForCom(fbId,comId,isLike);
+            }else{
+                auth(voteForCom.bind(this, fbId,comId,isLike));
+            }*/
+        });
+    });
+}
+var voteForCom = function(fbId,comId,like){
+    let weight;
+    (like == 1)? weight = 10000 : weight = -10000;
+    weight = updateVoteState(fbId,comId,weight/10000);
+    /*golos.broadcast.vote(wif, username, getComAuthor(fbId,comId), getPermlink(fbId,comId), weight, function(err, result){
+        console.log(err, result);
+    });*/
+}
 
 var getBtnVote = function(fbId, comId, isLike){
     let btn;
@@ -363,6 +466,15 @@ var getBtnVote = function(fbId, comId, isLike){
     }
     return btn;
 }
+var getBtnsVote = function(fbId,comId){
+    if(comId){
+        return Array.from(getComment(fbId,comId).getElementsByClassName('btn-com-vote'));
+    }else{
+        return Array.from( getFbControls(fbId).getElementsByClassName('btn-vote'));
+    }
+}
+
+
 
 
 //OTHER FUNCTIONS
@@ -374,7 +486,28 @@ var delClassIfContains = function (element, className){
     }
 }
 
-function getPermlink(fbId,comId){
+/*Calculates and sets a new vote state of a feedback or comment depending of their state and given vote*/
+var updateVoteState = function(fbId,comId,vote){
+    let state = getVoteState(fbId,comId);
+    let res;
+    
+    //setting up new state depending on the pressed button
+    if(vote == -1 && state != -1) res = -1;
+    if(vote == 1 && state != 1) res = 1
+    if(vote*state>0) res = 0;
+    
+    if(comId){
+        getComment(fbId,comId).setAttribute('data-like',res);
+    }else{
+        document.getElementById(fbId).setAttribute('data-like',res);
+    }
+    setLblVote(fbId,comId,state,res);
+    checkVoteColor(fbId,comId);
+    return res*10000;
+}
+
+/*Gets the permlink parameter of the given feedback or comment*/
+var getPermlink = function(fbId,comId){
     let result;
     if(comId){
         result = getComment(fbId,comId).getAttribute('data-permlink');
@@ -383,7 +516,9 @@ function getPermlink(fbId,comId){
     }
     return result;
 }
-function getAuthor(fbId,comId){
+
+/*Gets the author's name of the given feedback of comment*/
+var getAuthor = function(fbId,comId){
     let result;
     if(comId){
         //result = getComment(fbId,comId).getAttribute('data-permlink');
@@ -393,3 +528,81 @@ function getAuthor(fbId,comId){
     return result;
 }
 
+/*Gets the current vote state of a feedback or comment*/
+var getVoteState = function(fbId,comId){
+    let state;
+    if(comId){
+        state = Number(getComment(fbId,comId).getAttribute('data-like'));
+    }else{
+        state = Number(document.getElementById(fbId).getAttribute('data-like'));
+    }
+    return state;
+}
+
+/*Gets the vote state relatively to the current user if he has signed in*/
+var getVoteStateOnload = function(object){
+    let result=0;
+    /*if(wif){
+        object.active_votes.forEach(function(item){
+            if(item.voter==username){
+                result = item.percent;
+            }
+        });
+    }else{
+        result = 0;
+    }*/
+    return result;
+}
+
+/*Sets the number of likes or dislikes on the label in a comment or feedback control panel*/
+//упростить
+var setLblVote = function(fbId,comId,val0,val){
+    let likes;
+    let dislikes;
+    let label;
+    if(val==0 || val0==0){//одиночные изменения
+        
+        //нажатие на дизлайк
+        if(val == -1 || val0 == -1) label = getBtnVote(fbId,comId,0).nextElementSibling;
+        //нажатие на лайк
+        if(val == 1 || val0 == 1) label = getBtnVote(fbId,comId,1).previousElementSibling;
+        
+        likes = Number(label.innerHTML);
+        
+        //изменение числа
+        if(val==0) likes--;
+        if(val0==0) likes++;
+        label.innerHTML = likes;
+        
+    }else if(val != 0 && val0 != 0){
+        //двойное изменение
+        likes = Number(getBtnVote(fbId,comId,1).previousElementSibling.innerHTML);
+        dislikes = Number(getBtnVote(fbId,comId,0).nextElementSibling.innerHTML);
+        if(val==1){
+            //нажатие на лайк
+            likes++;
+            dislikes--;
+        }else{
+            //нажатие на дизлайк
+            likes--;
+            dislikes++;
+        }
+        getBtnVote(fbId,comId,1).previousElementSibling.innerHTML = likes;
+        getBtnVote(fbId,comId,0).nextElementSibling.innerHTML = dislikes;
+    }
+}
+
+/*checks the color of vote buttons and sets it according to actual vote state*/
+var checkVoteColor = function(fbId,comId){
+    let state = getVoteState(fbId,comId);
+    if(state == -1){
+        delClassIfContains(getBtnVote(fbId,comId,1),'btn-success');
+        getBtnVote(fbId,comId,0).classList.add('btn-danger');
+    }else if(state == 0){
+        delClassIfContains(getBtnVote(fbId,comId,1),'btn-success');
+        delClassIfContains(getBtnVote(fbId,comId,0),'btn-danger');
+    }else if(state == 1){
+        delClassIfContains(getBtnVote(fbId,comId,0),'btn-danger');
+        getBtnVote(fbId,comId,1).classList.add('btn-success');
+    }
+}
