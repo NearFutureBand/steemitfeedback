75a7a1b25981c7686b3e001e3b7a3b99687e9369
Paul White <emberprime43@gmail.com>
2018-05-31 23:47:17 +0300

    adding urlToLit function for permlinks

--- js/api.js
+++ js/api.js
@@ -132,7 +132,7 @@ var sendAddFbForm = function() {
     let parentPermlink = 'fb';
     let author = username;
     let title = document.getElementById('formHeader').value;
-    let permlink = 'post-' + parentPermlink.split(' ')[0] + '-' + Date.now().toString();
+    let permlink = urlLit( title, 0 ) + ' ' + Date.now().toString();
     let body = ckeditor.getData();
     //const body = formText.getData();
     
@@ -834,4 +834,17 @@ var resetHash = function() {
 }
 var clearHash = function() {
     location.hash = '';
+}
+
+var urlLit = function(w, v) { // cyrilic-to-translit-function
+	var tr = 'a b v g d e ["zh","j"] z i y k l m n o p r s t u f h c ch sh ["shh","shch"] ~ y ~ e yu ya ~ ["jo","e"]'.split(' ');
+	var ww = '';
+	w = w.toLowerCase();
+	for (i = 0; i < w.length; ++i) {
+		cc = w.charCodeAt(i);
+		ch = (cc >= 1072 ? tr[cc - 1072] : w[i]);
+		if (ch.length < 3) ww += ch;
+		else ww += eval(ch)[v];
+	}
+	return (ww.replace(/[^a-zA-Z0-9\-]/g, '-').replace(/[-]{2,}/gim, '-').replace(/^\-+/g, '').replace(/\-+$/g, ''));
 }
