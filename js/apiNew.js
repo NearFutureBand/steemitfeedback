9dd2660a1f0d64b36ff6eb37894b35abaef624e0
Paul White <theflashofsonic@gmail.com>
2018-09-29 21:56:12 +0300

    g.r. step 4: rendering feedbacks

--- js/apiNew.js
+++ js/apiNew.js
@@ -1,5 +1,5 @@
 /*
-    makeHTML() - returns HTML code of the exemplar to build in it somewhere
+    makeDynHTML() - returns HTML code of the exemplar that will be constantly changing
     getThisEl() - returns js variable of the exemplar by using querySelector
     place() - places the exemplar to the DOM structure, it is executing only one time
     restate() - rebuilds the structure of the exemplar with updated dynamic parameters
@@ -28,7 +28,10 @@ class FilterTab {
     }
     
     /*Events*/
-    
+    click() {
+        this.active = true;
+        return this.key;
+    }
 }
 
 /*Navigation bar with tabs with counters for filtering incoming feedbacks*/
@@ -45,11 +48,20 @@ class Filter {
     
     init() {
         this.place();
-        this.addEvntClk();
     }
     
-    makeHTML() {
-        let exportHTML = 
+    makeDynHTML() {
+        let exportHTML = '';
+        this.tabs.forEach( function(tab) {
+            exportHTML += ('<li class="nav-item">'+ tab.makeHTML() + '</li>');
+        });
+        return exportHTML;
+    }
+    
+    place() {
+        let el = document.createElement('div');
+        el.className = 'row ' + this.className;
+        el.innerHTML = 
             '<div class="col-12">'+
                 '<nav class="navbar navbar-expand-lg tabs">'+
                     '<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavFeedbackTabs" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">'+
@@ -59,35 +71,23 @@ class Filter {
                         '<div class="container">'+
                             '<div class="row">'+
                                 '<div class="col-12 tabs">'+
-                                    '<ul class="nav nav-tabs">';
-        this.tabs.forEach( function(tab) {
-            exportHTML += ('<li class="nav-item">'+ tab.makeHTML() + '</li>');
-        });
-        exportHTML += (
+                                    '<ul class="nav nav-tabs">'+
+                                        
                                     '</ul>'+
                                 '</div>'+
                             '</div>'+
                         '</div>'+
                     '</div>'+
                 '</nav>'+
-            '</div>'
-        );
-        return exportHTML;
-    }
-    
-    place() {
-        let el = document.createElement('div');
-        el.className = 'row ' + this.className;
-        //el.innerHTML = ''
+            '</div>';
         document.querySelector('.' + this.GFCLASS).appendChild(el);
         this.restate();
     }
     
     restate() {
-        let el = this.getThisEl();
+        let el = this.getDynBlock();
         if( el.innerHTML != '') el.innerHTML = '';
-        el.innerHTML = this.makeHTML();
-        
+        el.innerHTML = this.makeDynHTML();
         this.addEvntClk();
     }
     
@@ -97,6 +97,7 @@ class Filter {
         for( let i = 0; i < this.tabs.length; i++) {
             this.tabs[i].getThisEl().addEventListener('click', function() {
                 $.makeTabActive(i);
+                console.log($.currentFbSelector);
                 $.restate();
             });
         }
@@ -105,6 +106,9 @@ class Filter {
     /*Incapsulated*/
     getThisEl() {
         return document.querySelector('.'+ this.GFCLASS +' .row.'+ this.className);
+    }
+    getDynBlock() {
+        return document.querySelector('.'+ this.GFCLASS +' .row.'+ this.className +' ul.nav.nav-tabs');
     }
     createFilterTabs(GFCLASS) {
         this.tabs.push( new FilterTab('All', 'all', 'icon-radio-unchecked', GFCLASS));
@@ -117,9 +121,11 @@ class Filter {
         for(let i = 0; i < this.tabs.length; i++) {
             this.tabs[i].active = false;
         }
-        this.tabs[index].active = true;
         this.activeTab = index;
-        this.currentFbSelector[0] = this.tabs[index].key;
+        this.currentFbSelector[0] = this.tabs[index].click();
+    }
+    incCounter(index) {
+        
     }
     getTabByKey(key) {
         for( let i = 0; i < this.tabs.length; i++) {
@@ -128,10 +134,76 @@ class Filter {
         console.log('getTabByKey function error: not found element with this key');
         return null;
     }
+    
 }
 
 class Feedback {
-
+    constructor(id, permlink, type, heading, body, author, date, GFCLASS) {
+        this.className = 'fb';
+        this.expanded = false;
+        this.yourVote = 0;
+        this.GFCLASS = GFCLASS;
+        this.id = id;
+        this.type = type;
+        this.permlink = permlink;
+        this.body = body;
+        this.heading = heading;
+        this.date = date;
+        this.author = author;
+        this.comments = [];
+        this.likes = 0;
+        this.dislikes = 0;
+    }
+    place() {
+        let el = document.createElement('div');
+        el.className = 'row ' + this.className;
+        el.setAttribute('id', 'fb-' + this.id);
+        
+        el.innerHTML = 
+            '<div class="container body-fb tile">'+
+                '<div class="row">'+
+                    '<div class="col-lg-9 col-md-9 text">'+
+                        '<h3>'+ this.heading +'</h3>'+
+                        '<p>'+ this.body +'</p>'+
+                        '<div class="buttons">'+
+                            '<button type="button" class="btn btn-dark btn-show-comments">'+
+                                '<span class="badge badge-light">'+ this.comments.length +'</span>'+
+                                '<span class="icon-message-square"></span>'+
+                                '<span class="icon-arrow-left hidden"></span>'+
+                                '<span class="hidden"> Back</span>'+
+                            '</button>'+
+                        '</div>'+
+                    '</div>'+
+                    '<div class="col-lg-3 col-md-3 controls">'+
+                        '<div class="controls-wrapper">'+
+                            '<div class="name">'+
+                                '<h6>'+ this.author +'</h6>'+
+                            '</div>'+
+                            '<div class="photo">'+
+                                '<img src="http://www.xn--80aefdbw1bleoa1d.xn--p1ai//plugins/uit/mychat/assets/img/no_avatar.jpg">'+
+                            '</div>'+
+                            '<div class="date">'+
+                                '<small>'+ this.date +'</small>'+
+                            '</div>'+
+                            '<div class="likes">'+
+                                '<button type="button" class="btn btn-secondary btn-vote" data-like="1">'+
+                                    '<span class="badge badge-dark">'+ this.likes +'</span>'+
+                                    '<span class="icon-thumbs-up"></span>'+
+                                '</button>'+
+                                '<button type="button" class="btn btn-secondary btn-vote" data-like="0">'+
+                                    '<span class="icon-thumbs-down"></span>'+
+                                    '<span class="badge badge-dark">'+ this.dislikes +'</span>'+
+                                '</button>'+
+                            '</div>'+
+                        '</div>'+
+                    '</div>'+
+                '</div>'+
+            '</div>'+
+            '<div class="container comments"></div>';
+        console.log('.' + this.GFCLASS);
+        document.querySelector('.' + this.GFCLASS).appendChild(el);
+        /*<div class="row fb" id="381" data-permlink="thank-1537600262697" data-opened="0" data-like="0"><div class="container body-fb tile"><div class="row"><div class="col-lg-9 col-md-9 text"><h3>thank</h3><p></p><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eget mi dapibus, consequat mauris eget, rutrum diam. Vivamus in velit at libero vestibulum faucibus vel vitae enim. Suspendisse feugiat, enim vel aliquet volutpat, sem risus molestie neque, non faucibus nisl massa sit amet purus. Aenean pharetra, nibh sit amet venenatis pretium, elit libero tempus mauris, bibendum imperdiet diam ...</p><div class="buttons"><button type="button" class="btn btn-dark btn-show-comments"><span class="badge badge-light">0</span><span class="icon-message-square"></span><span class="icon-arrow-left hidden"></span><span class="hidden"> Back</span></button></div></div><div class="col-lg-3 col-md-3 controls"><div class="controls-wrapper"><div class="name"><h6>test4</h6></div><div class="photo"><img src="http://www.xn--80aefdbw1bleoa1d.xn--p1ai//plugins/uit/mychat/assets/img/no_avatar.jpg"></div><div class="date"><small>September 22nd 2018, 7:11:24 am</small></div><div class="likes"><button type="button" class="btn btn-secondary btn-vote" data-like="1"><span class="badge badge-dark">0</span><span class="icon-thumbs-up"></span></button><button type="button" class="btn btn-secondary btn-vote" data-like="0"><span class="icon-thumbs-down"></span><span class="badge badge-dark">0</span></button></div></div></div></div></div><div class="container comments"></div></div>*/
+    }
 }
 class Comment {
     
@@ -145,10 +217,15 @@ class GolosFeedback {
         this.className = 'golos-feedback-container';
         this.initBootstrapStructure();
         this.navbar2 = new Filter(this.className);
+        this.feedbacks = [];
+        this.feedbacks.push( new Feedback(0,'blablabla','idea' ,'title','Lorem ipsum dolor sit amet, consectetur adipiscing elit.','author', '28-04-2018', this.className));
+        this.feedbacks.push( new Feedback(1,'eeeeeeeeeee','thank' ,'feedback here','Aliquam eget mi dapibus, consequat mauris eget, rutrum diam.','Vasya', '03-06-2018', this.className));
     }
     
     init() {
         this.navbar2.init();
+        this.feedbacks[0].place();
+        this.feedbacks[1].place();
     }
     
     initBootstrapStructure() {
